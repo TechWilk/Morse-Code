ffdc441233c559ffc4a6949f5791588d79a02243
Christopher Wilkinson <git@wilk.tech>
2017-01-11 10:48:53 +0000

    Added tap button and labels (labels off screen - need fixing)

--- morse code/GameScene.swift
+++ morse code/GameScene.swift
@@ -14,12 +14,18 @@ class GameScene: SKScene {
     
     private var tonePlayer = AVAudioPlayer()
     
+    public var unit = 1.0 // length of a dit / space
+    //public var gapUnit = unit
+    
     
     let dit = CGRect(x: 100.0, y: 100.0, width: 100.0, height: 10.0)
     let dah = CGRect(x: 100.0, y: 100.0, width: 10.0, height: 10.0)
     
     var chars = [String: Array<Any>]()
     
+    let tapButton = SKSpriteNode()
+    var wordsPerMinLabel = SKLabelNode()
+    var timeLabel = SKLabelNode()
     
     
     override func didMove(to view: SKView) {
@@ -68,6 +74,11 @@ class GameScene: SKScene {
         _ = try? tonePlayer = AVAudioPlayer(contentsOf: toneFileUrl as URL)
         tonePlayer.numberOfLoops = -1 // loop indefinitley
         tonePlayer.prepareToPlay()
+        
+        self.backgroundColor = SKColor(red: 60/255, green: 173/255, blue: 237/255, alpha: 1)
+        
+        setupButtons()
+        setupLabels()
     }
     
     
@@ -77,10 +88,63 @@ class GameScene: SKScene {
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        tonePlayer.play()
+        
+        for touch in touches {
+            if tapButton.frame.contains(touch.location(in: self)) {
+                tonePlayer.play()
+                // todo: start tapTimer
+                (tapButton.childNode(withName: "tapButtonInner") as! SKSpriteNode).color = UIColor(red: 160/255, green: 225/255, blue: 255/255, alpha: 1)
+                
+            }
+        }
     }
     
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        tonePlayer.stop()
+        
+        for touch in touches {
+            if tapButton.frame.contains(touch.location(in: self)) {
+                // todo: end tapTimer
+                // todo: calculate if dit or dah was entered
+                run(SKAction.sequence([SKAction.wait(forDuration: 0.005), SKAction.run {
+                    self.tonePlayer.stop()
+                    self.tonePlayer.prepareToPlay()
+                    (self.tapButton.childNode(withName: "tapButtonInner") as! SKSpriteNode).color = UIColor(red: 194/255, green: 234/255, blue: 255/255, alpha: 1)
+                    }]))
+            }
+        }
+    }
+    
+    
+    /* ~~~~~~~~~~~ SETUP ~~~~~~~~~~ */
+    
+    func setupButtons() {
+        tapButton.size = CGSize(width: 100, height: 100)
+        tapButton.position = CGPoint(x: (frame.midX - frame.width/4), y: frame.midY)
+        tapButton.color = UIColor(red: 115/255, green: 220/255, blue: 255/255, alpha: 1)
+        tapButton.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        addChild(tapButton)
+        
+        let tapButtonInner = SKSpriteNode(color: UIColor(red: 194/255, green: 234/255, blue: 255/255, alpha: 1), size: tapButton.frame.insetBy(dx: 10, dy: 10).size)
+        tapButtonInner.position = CGPoint(x: 0, y: 0 )
+        tapButtonInner.name = "tapButtonInner"
+        tapButtonInner.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        tapButton.addChild(tapButtonInner)
+    }
+    
+    func setupLabels() {
+        let wordsPerMin = Int((unit * 18) / 60)
+        wordsPerMinLabel.text = "\(wordsPerMin) wpm"
+        wordsPerMinLabel.position = CGPoint(x: frame.maxX - 10, y: frame.minY + 10)
+        wordsPerMinLabel.horizontalAlignmentMode = .right
+        wordsPerMinLabel.verticalAlignmentMode = .bottom
+        addChild(wordsPerMinLabel)
+        
+        timeLabel.text = "time"
+        timeLabel.position = CGPoint(x: frame.maxX - 10, y: (frame.minY + 10 + wordsPerMinLabel.fontSize + 10) )
+        timeLabel.horizontalAlignmentMode = .right
+        timeLabel.verticalAlignmentMode = .bottom
+        addChild(timeLabel)
+        
     }
 }
